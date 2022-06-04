89efddb8285844c9b3da0aac76722e0ca55350d5
cherifb16 <cherifbobbyray@gmail.com>
2022-06-04 13:37:59 +0200

    test

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,11 +9,12 @@ class OrdersController < ApplicationController
     @items = Item.all.order(:created_at)
   end
 
+  
   def create
     @order = current_user.orders.build(order_params)
-    @order.update_total_quantity
-    #update_total_quantityメソッドは、注文された発注量を総量に反映するメソッドであり、Orderモデルに定義されています。
     @order.save
+    @order.update_total_quantity
+    # update_total_quantityメソッドは、注文された発注量を総量に反映するメソッドであり、Orderモデルに定義されています。
     redirect_to orders_path
   end
 
